885525b2aa7c68bbd5a7bff98cfd61bd3856ebb1
Maria João <75786385+mariajoao722@users.noreply.github.com>
2023-12-03 22:42:53 +0000

    Script MON

--- scripts/scriptMON.sh
+++ scripts/scriptMON.sh
@@ -1,32 +1,34 @@
-
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install -y ceph
 sudo apt-get install -y openssh-client
+
 uui=$(uuidgen)
+HOSTNAME=$(hostname)
+IP_ADDR=$(hostname -I)
+
 # Create or update ceph.conf
 cat <<EOF > /etc/ceph/ceph.conf
 [global]
 # specify cluster network for monitoring
 cluster network = 10.204.0.0/24
+
 # specify public network
 public network = 10.204.0.0/24
+
 # specify UUID generated above
 fsid = $uui
-# specify IP address of Monitor Daemon
+
+# specify IP address of Monitor DaemonS
 mon host = 10.204.0.12
+
 # specify Hostname of Monitor Daemon
-mon initial members = node01
+mon initial members = $HOSTNAME
 osd pool default crush rule = -1
 
-# mon.(Node name)
-[mon.node01]
-# specify Hostname of Monitor Daemon
-host = node01
-# specify IP address of Monitor Daemon
-mon addr = 10.0.0.12
-# allow to delete pools
-mon allow pool delete = true
+auth_cluster required = cephx
+auth_service required = cephx
+auth_client required = cephx
 EOF
 
 # Generate secret key for Cluster monitoring
@@ -48,18 +50,26 @@ NODENAME=$(grep "^mon initial" /etc/ceph/ceph.conf | awk {'print $NF'})
 NODEIP=$(grep "^mon host" /etc/ceph/ceph.conf | awk {'print $NF'})
 monmaptool --create --add $NODENAME $NODEIP --fsid $FSID /etc/ceph/monmap
 
+outfile=/home/mjmarquespais/debug.txt
+
+echo $HOSTNAME | tee -a $outfile  # mon
+echo $uui | tee -a $outfile
+echo $FSID | tee -a $outfile
+echo $NODENAME | tee -a $outfile  # mon
+echo $NODEIP | tee -a $outfile
+
 # create a directory for Monitor Daemon
 # directory name ⇒ (Cluster Name)-(Node Name)
-sudo mkdir /var/lib/ceph/mon/ceph-node01
+sudo mkdir /var/lib/ceph/mon/ceph-mon
 
 # assosiate key and monmap to Monitor Daemon
 # --cluster (Cluster Name)
 sudo ceph-mon --cluster ceph --mkfs -i $NODENAME --monmap /etc/ceph/monmap --keyring /etc/ceph/ceph.mon.keyring
 sudo chown ceph. /etc/ceph/ceph.*
-sudo chown -R ceph. /var/lib/ceph/mon/ceph-node01 /var/lib/ceph/bootstrap-osd
+sudo chown -R ceph. /var/lib/ceph/mon/ceph-mon /var/lib/ceph/bootstrap-osd
 sudo systemctl enable --now ceph-mon@$NODENAME
 
-outfile=/home/mjmarquespais/debug.txt
+
 # enable Messenger v2 Protocol
 echo "before" | tee -a $outfile
 sudo ceph mon enable-msgr2 | tee -a $outfile
@@ -68,19 +78,27 @@ sudo ceph config set mon auth_allow_insecure_global_id_reclaim false
 
 # create a directory for Manager Daemon
 # directory name ⇒ (Cluster Name)-(Node Name)
-sudo mkdir /var/lib/ceph/mgr/ceph-node01
+sudo mkdir /var/lib/ceph/mgr/ceph-mon
 
+echo "after1" | tee -a $outfile
 # create auth key
 sudo ceph auth get-or-create mgr.$NODENAME mon 'allow profile mgr' osd 'allow *' mds 'allow *'
 
+echo "after2" | tee -a $outfile
 
-sudo ceph auth get-or-create mgr.node01 | tee /etc/ceph/ceph.mgr.admin.keyring
-sudo cp /etc/ceph/ceph.mgr.admin.keyring /var/lib/ceph/mgr/ceph-node01/keyring
+sudo ceph auth get-or-create mgr.mon | tee /etc/ceph/ceph.mgr.admin.keyring
+sudo cp /etc/ceph/ceph.mgr.admin.keyring /var/lib/ceph/mgr/ceph-mon/keyring
 sudo chown ceph. /etc/ceph/ceph.mgr.admin.keyring
-sudo chown -R ceph. /var/lib/ceph/mgr/ceph-node01
+sudo chown -R ceph. /var/lib/ceph/mgr/ceph-mon
 sudo systemctl enable --now ceph-mgr@$NODENAME
 
-chown ceph. /etc/ceph/ceph.* /var/lib/ceph/bootstrap-osd/*; \
-parted --script /dev/sdb 'mklabel gpt'; \
-parted --script /dev/sdb "mkpart primary 0% 100%"; \
-ceph-volume lvm create --data /dev/sdb1
+echo "after3" | tee -a $outfile
+
+#sudo chown -R mjmarquespais:mjmarquespais /var/lib/ceph/mon/ceph-mon
+#sudo chown -R mjmarquespais:mjmarquespais /var/lib/ceph/mgr/ceph-mon
+
+
+#chown ceph. /etc/ceph/ceph.* /var/lib/ceph/bootstrap-osd/*; \
+#parted --script /dev/sdb 'mklabel gpt'; \
+#parted --script /dev/sdb "mkpart primary 0% 100%"; \
+#ceph-volume lvm create --data /dev/sdb1
